92716ce4d75044f092814a9c3014e3646bcbacdd
Nacho <ignaciocastroplanas@gmail.com>
2023-05-11 09:53:57 -0300

    CORS cambiado

--- src/main/java/com/back/AuthApp/controller/EducacionController.java
+++ src/main/java/com/back/AuthApp/controller/EducacionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-ignacio.web.app")
 public class EducacionController {
     
     @Autowired
--- src/main/java/com/back/AuthApp/controller/UserController.java
+++ src/main/java/com/back/AuthApp/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-ignacio.web.app")
 public class UserController {
 
     @Autowired
